effd2446b546b1da76ca4943efb9e3bcbd2797d6
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-03-30 01:24:45 +0600

    Hambarg menu with a little bit javascript and CSS

---
+++ Transitions Animation A Little JavaScript/practice/js and css/script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('button');
+// const button = document.querySelector('#button');
+
+const heading = document.querySelector('h1')
+
+
+button.addEventListener('click', function () {
+
+    document.body.classList.toggle('dark')
+    heading.classList.toggle('yellow')
+
+
+})
